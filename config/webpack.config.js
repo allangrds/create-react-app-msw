4dee2fd409f8bc7f92f9f5d71e8557aff5581d3f
Allan Ramos <allangrds@gmail.com>
2023-01-04 12:00:28 -0300

    chore: add webpack bundle analyser

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -25,6 +25,7 @@ const ForkTsCheckerWebpackPlugin =
     ? require('react-dev-utils/ForkTsCheckerWarningWebpackPlugin')
     : require('react-dev-utils/ForkTsCheckerWebpackPlugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const createEnvironmentHash = require('./webpack/persistentCache/createEnvironmentHash');
 
@@ -747,6 +748,7 @@ module.exports = function (webpackEnv) {
             },
           },
         }),
+      new BundleAnalyzerPlugin(),
     ].filter(Boolean),
     // Turn off performance processing because we utilize
     // our own hints via the FileSizeReporter
